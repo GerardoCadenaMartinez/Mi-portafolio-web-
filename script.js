17a0e3f222afce27a166695ef3a859387829c367
GeDEV <47062065+GerardoCadenaMartinez@users.noreply.github.com>
2025-06-30 23:17:37 -0600

    Add files via upload

--- script.js
+++
@@ -1,8 +0,0 @@
-// Inicializa ScrollReveal para animar elementos con clase .card
-ScrollReveal().reveal('.card', {
-  duration: 1000,      // Duración de la animación en milisegundos
-  distance: '50px',    // Distancia que se mueve el elemento durante la animación
-  origin: 'bottom',    // Desde dónde aparece el elemento (puede ser 'top', 'left', 'right', 'bottom')
-  easing: 'ease-in-out',
-  reset: false         // Si es false, la animación no se repite cuando haces scroll arriba y abajo
-});
